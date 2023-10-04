038baa4930bde282908948fe7611cd3d3f94ec90
timothyscherer <tjtractorboy@gmail.com>
2023-10-04 01:59:23 +0000

    finished up to quiz2.rs

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -5,10 +5,10 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 fn main() {
-    let a = ???
+    let a: [i32; 100] = [0; 100]; // [0; 100] is an array of 100 elements, all initialized to 0
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
